6e53f5714a0bc4489124f39dd6359feb7a5e942a
jjcmerin <jjc.merin@mail.ru>
2023-06-26 15:17:53 +0700

    update(): use db, game fixed

--- handlers.py
+++ handlers.py
@@ -1,14 +1,14 @@
+from random import randint
+
 from aiogram import Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
-from utils import get_random_number, user
+from db.requests.user_requests import get_user_attempts, get_user_stats, add_user_total_games, add_user_wins
 
 
 class FSMGame(StatesGroup):
-    in_choice = State()
-    start_game = State()
     game = State()
 
 
@@ -16,46 +16,47 @@ router = Router()
 
 
 @router.message(Command('stats'))
-async def end_game(message: types.Message):
-    await message.answer(f'Всего игр: {user["total_games"]}\n'
-                         f'Побед: {user["wins"]}')
+async def get_stats(message: types.Message):
+    games, wins = await get_user_stats(message.from_user.id)
+
+    await message.answer(f'Всего игр: {games}\n'
+                         f'Побед: {wins}')
 
 
 @router.message(Command('start'))
-async def start(message: types.Message, state: FSMContext):
-    await message.answer(f'Давай сыграем в игру, {message.from_user.first_name}! '
-                         f'Я загадаю число от 1 до 5, а ты попытаешься угадать.\n'
-                         f'Напиши "go", чтобы сыграть или любое сообщение, чтобы отказаться.')
-    await state.set_state(FSMGame.in_choice)
+async def start(message: types.Message):
+    await message.answer(f'Давай сыграем в игру, {message.from_user.first_name}. '
+                         f'Я загадаю число от 1 до 100, а ты попытаешься угадать.\n'
+                         f'Напиши /game, чтобы сыграть. Напиши /stats, чтобы узнать свой счет.')
 
 
-@router.message(FSMGame.in_choice)
+@router.message(Command('game'))
 async def start_game(message: types.Message, state: FSMContext):
-    if message.text == 'go':
-        user['secret_number'] = get_random_number()
-        await message.answer(f'И так, как ты думаешь, что я загадал? Введи число.')
-        user['attempts'] = 2
-        await state.set_state(FSMGame.start_game)
-    else:
-        await message.answer(f'Когда захочешь сыграть, просто напиши "go".')
-        await state.set_state(FSMGame.in_choice)
+    await state.update_data(secret_number=randint(1, 100))
+    await state.update_data(attempts=await get_user_attempts(message.from_user.id))
 
-
-@router.message(FSMGame.start_game)
-async def game(message: types.Message, state: FSMContext):
     await state.set_state(FSMGame.game)
+    await message.answer(f'И так, как ты думаешь, что я загадал? Введи число.')
 
-    if user['attempts']:
-        if int(message.text) == user['secret_number']:
-            await message.answer(f"Верно, это {user['secret_number']}!")
-            user['wins'] += 1
-            await state.set_state(FSMGame.in_choice)
-        else:
-            user['attempts'] -= 1
-            await message.answer(f'Попробуй еще раз')
-            await state.set_state(FSMGame.start_game)
+
+@router.message(FSMGame.game)
+async def game(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    print(data)
+    if int(message.text) != data['secret_number']:
+        await state.update_data(attempts=data['attempts'] - 1)
+        data = await state.get_data()
+        await message.answer(f'Неверно. Оставшиеся попытки: {data["attempts"]}.')
+
+        if data['attempts'] == 0:
+            await add_user_total_games(message.from_user.id)
+            await message.answer(f'Game over. Напиши /game, чтобы сыграть. Напиши /stats, чтобы узнать свой счет.')
+            await state.clear()
+
     else:
-        await message.answer(f'Game over')
-        await state.set_state(FSMGame.in_choice)
+        await message.answer(f"Верно, это {data['secret_number']}.")
+        await add_user_wins(message.from_user.id)
+        await add_user_total_games(message.from_user.id)
+        await message.answer(f'Молодец! Напиши /game, чтобы сыграть. Напиши /stats, чтобы узнать свой счет.')
 
-    user['total_games'] += 1
+        await state.clear()
