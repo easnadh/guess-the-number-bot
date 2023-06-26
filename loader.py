77c73ef52d41cb9df8b6d225df623d898702d44e
jjcmerin <jjc.merin@mail.ru>
2023-06-26 15:15:55 +0700

    update(): add storage

--- loader.py
+++ loader.py
@@ -1,9 +1,11 @@
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import load_config
 
 config = load_config()
+storage = MemoryStorage()
 
 bot: Bot = Bot(token=config.tg_bot.token.get_secret_value(), parse_mode='HTML')
 
-dp: Dispatcher = Dispatcher()
+dp: Dispatcher = Dispatcher(storage=storage)
